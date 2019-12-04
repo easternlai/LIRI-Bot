cd74e424b88ae707404762c94e230ab95d3030b3
Eastern Lai <easternlai@hotmail.com>
2019-12-03 20:22:08 -0800

    created all files

---
+++ keys.js
@@ -0,0 +1,9 @@
+//Client ID d66e18f631f54277a9b21f21b3828546
+//Client Secret 4a367082aff04afea486f5f4a94593d5 
+
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,2 @@
+requestAnimationFrame("dotenv").config();
+
