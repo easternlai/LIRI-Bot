053a01493627663255fdb8d022328ca542d7c866
Eastern Lai <easternlai@hotmail.com>
2019-12-04 22:28:55 -0800

    created concert and spotify

--- liri.js
+++ liri.js
@@ -1,83 +1,52 @@
 require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
-var inquirer = require("inquirer");
-var userResponse = process.argv[2];
-var userArgs = process.argv[3];
+var command = process.argv[2];
+var term = process.argv.slice(3).join(" ");
 
-processAnswer();
+processArg();
 
-function firstQuestion(){
-    inquirer.prompt([
-        {
-        name: "question",
-        message: "Enter a command"
-        }
-    ]).then(function(input){
-        console.log(input.question);
-        response = input.question;
-        processAnswer();
-
-    });
-};
-
-function argChecker(){
-    if (userArgs !== undefined){
-        return userArgs;
-    }else{
-        inquirer.prompt([
-            {
-            name: "search",
-            message: "Search:"
-            }
-        ]).then(function(input){
-            console.log(input.search);
-            return input.search;
-            
-    
-    }
-}
-
-function processAnswer (){
-    switch(userResponse){
+function processArg (){
+    switch(command){
         case "concert-this":
-            var band = argChecker();
             axios
-            .get("https://rest.bandsintown.com/artists/"+ band +"/events?app_id=codingbootcamp")
+            .get("https://rest.bandsintown.com/artists/"+ term +"/events?app_id=codingbootcamp")
             .then(function(response) {
                 // If the axios was successful...
                 // Then log the body from the site!
-                console.log(response.data);
+                console.log("Band: ", term);
+                console.log("Location: ",response.data[0].venue.name);
+                console.log("Date: ", moment().format(response.data[0].datetime.slice(0, 10), "MM-DD-YYYY"));
             })
             .catch(function(error) {
                 if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
+                // console.log(error.response.data);
+                // console.log(error.response.status);
+                // console.log(error.response.headers);
                 } else if (error.request) {
                 // The request was made but no response was received
                 // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
+                // console.log(error.request);
                 } else {
                 // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
+                // console.log("Error", error.message);
                 }
-                console.log(error.config);
+                // console.log(error.config);
             });
 
             break;
         case "spotify-this-song":
-            
             spotify
-            .search({ type: 'track', query: 'All the Small Things' })
+            .search({type: 'track', query: term})
             .then(function(response) {
-                console.log(response);
+                console.log(response.tracks.items);
             })
             .catch(function(err) {
                 console.log(err);
@@ -86,11 +55,11 @@ function processAnswer (){
             break;
         case "movie-this":
             axios
-            .get("http://http://www.omdbapi.com/?i=tt0485947&apikey=74a7aa11")
+            .get("http://http://www.omdbapi.com/?i=" + term + "&apikey=74a7aa11")
             .then(function(response) {
                 // If the axios was successful...
                 // Then log the body from the site!
-                console.log("test");
+                console.log(response.data);
             })
             .catch(function(error) {
                 if (error.response) {
@@ -115,16 +84,9 @@ function processAnswer (){
                     return console.log(data);
                 }
                 var text = data;
-                console.log(text);
             });
             
             break;        
-        default:
-            if(userResponse!==undefined){
-                console.log("incorrect syntax!");
-            }
-            firstQuestion();
-
 
     }
 }
